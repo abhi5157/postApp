0e481762d987587c6edb727aa4f4b573ae46c97f
Abhishek <abhishekjoshi035@.com>
2024-09-19 16:35:58 +0530

    fst

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <SocialMediaPost />
+  </main>
+</template>
+
+<script setup>
+import SocialMediaPost from './components/SocialMediaPost.vue'
+</script>
+
+<style>
+/* Add any global styles here */
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f0f2f5;
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// nuxt.config.ts
+export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+        }
+      ]
+    }
+  }
+})
